02ab34778f85fd9551bb95eb52e9c7dbbb119dac
RavinduSachintha <ravindusachintha53@gmail.com>
2019-10-04 02:28:06 +0530

    updated dashboard

--- FrontAngular/src/app/pages/dashboard/dashboard.component.ts
+++ FrontAngular/src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import Chart from 'chart.js';
+import { FireService } from 'src/app/services/fire.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-dashboard",
@@ -15,9 +17,34 @@ export class DashboardComponent implements OnInit {
   public clicked1: boolean = false;
   public clicked2: boolean = false;
 
-  constructor() {}
+  public curr_time: number;
+  public curr_light: number;
+  public curr_temp: number;
+  public curr_total_pwr: number;
+  public curr_light_pwr: number;
+  public curr_temp_pwr: number;
+
+  public timeLineList1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  public timeLineList2 = [0, 0, 0, 0, 0, 0];
+  public lightDataList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  public tempDataList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  public lightPwrDataList = [0, 0, 0, 0, 0, 0];
+  public tempPwrDataList = [0, 0, 0, 0, 0, 0];
+  public totalPwrDataList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  constructor(private fs: FireService, private router: Router) {
+    this.curr_time = 0;
+  }
 
   ngOnInit() {
+    this.fs.getCurrentEnv().subscribe((ce: any) => {
+      this.curr_light = ce.light;
+      this.curr_temp = ce.temperature;
+      this.curr_light_pwr = ce.output_lights;
+      this.curr_temp_pwr = ce.output_ac;
+      this.curr_total_pwr = (ce.output_lights + ce.output_ac) * 0.5;
+    });
+
     var gradientChartOptionsConfigurationWithTooltipBlue: any = {
       maintainAspectRatio: false,
       legend: {
@@ -44,10 +71,10 @@ export class DashboardComponent implements OnInit {
             zeroLineColor: "transparent",
           },
           ticks: {
-            suggestedMin: 60,
-            suggestedMax: 125,
-            padding: 20,
-            fontColor: "#2380f7"
+            suggestedMin: 0,
+            suggestedMax: 12,
+            padding: 2,
+            fontColor: "#9a9a9a"
           }
         }],
 
@@ -60,7 +87,7 @@ export class DashboardComponent implements OnInit {
           },
           ticks: {
             padding: 20,
-            fontColor: "#2380f7"
+            fontColor: "#9a9a9a"
           }
         }]
       }
@@ -140,9 +167,9 @@ export class DashboardComponent implements OnInit {
             zeroLineColor: "transparent",
           },
           ticks: {
-            suggestedMin: 60,
-            suggestedMax: 125,
-            padding: 20,
+            suggestedMin: 0,
+            suggestedMax: 12,
+            padding: 2,
             fontColor: "#9a9a9a"
           }
         }],
@@ -236,9 +263,9 @@ export class DashboardComponent implements OnInit {
             zeroLineColor: "transparent",
           },
           ticks: {
-            suggestedMin: 50,
-            suggestedMax: 125,
-            padding: 20,
+            suggestedMin: 0,
+            suggestedMax: 12,
+            padding: 2,
             fontColor: "#9e9e9e"
           }
         }],
@@ -317,8 +344,8 @@ export class DashboardComponent implements OnInit {
     gradientStroke.addColorStop(0.4, 'rgba(233,32,16,0.0)');
     gradientStroke.addColorStop(0, 'rgba(233,32,16,0)'); //red colors
 
-    var data = {
-      labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+    var data1 = {
+      labels: this.timeLineList2,
       datasets: [{
         label: "Data",
         fill: true,
@@ -334,13 +361,13 @@ export class DashboardComponent implements OnInit {
         pointHoverRadius: 4,
         pointHoverBorderWidth: 15,
         pointRadius: 4,
-        data: [80, 100, 70, 80, 120, 80],
+        data: this.lightPwrDataList,
       }]
     };
 
-    var myChart = new Chart(this.ctx, {
+    var myChart1 = new Chart(this.ctx, {
       type: 'line',
-      data: data,
+      data: data1,
       options: gradientChartOptionsConfigurationWithTooltipRed
     });
     // ------------ light power - output chart
@@ -356,10 +383,10 @@ export class DashboardComponent implements OnInit {
     gradientStroke.addColorStop(0.4, 'rgba(66,134,121,0.0)'); //green colors
     gradientStroke.addColorStop(0, 'rgba(66,134,121,0)'); //green colors
 
-    var data = {
-      labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
+    var data2 = {
+      labels: this.timeLineList2,
       datasets: [{
-        label: "My First dataset",
+        label: "Data",
         fill: true,
         backgroundColor: gradientStroke,
         borderColor: '#00d6b4',
@@ -373,24 +400,24 @@ export class DashboardComponent implements OnInit {
         pointHoverRadius: 4,
         pointHoverBorderWidth: 15,
         pointRadius: 4,
-        data: [90, 27, 60, 12, 80],
+        data: this.tempPwrDataList,
       }]
     };
 
-    var myChart = new Chart(this.ctx, {
+    var myChart2 = new Chart(this.ctx, {
       type: 'line',
-      data: data,
+      data: data2,
       options: gradientChartOptionsConfigurationWithTooltipGreen
 
     });
     // ------------ temp power - output chart
 
     // ------------ big chart
-    var chart_labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+    var chart_labels = this.timeLineList1;
     this.datasets = [
-      [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
-      [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
-      [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
+      this.lightDataList,
+      this.tempDataList,
+      this.totalPwrDataList
     ];
     this.data = this.datasets[0];
 
@@ -410,7 +437,7 @@ export class DashboardComponent implements OnInit {
       data: {
         labels: chart_labels,
         datasets: [{
-          label: "My First dataset",
+          label: "Data",
           fill: true,
           backgroundColor: gradientStroke,
           borderColor: '#1f8ef1',
@@ -427,15 +454,50 @@ export class DashboardComponent implements OnInit {
           data: this.data,
         }]
       },
-      options: gradientChartOptionsConfigurationWithTooltipRed
+      options: gradientChartOptionsConfigurationWithTooltipBlue
     };
     this.myChartData = new Chart(this.ctx, config);
     // ------------ big chart
+
+    setInterval(()=> {
+      this.lightDataList.shift();
+      this.lightDataList.push(this.curr_light);
+      this.tempDataList.shift();
+      this.tempDataList.push(this.curr_temp);
+      this.lightPwrDataList.shift();
+      this.lightPwrDataList.push(this.curr_light_pwr);
+      this.tempPwrDataList.shift();
+      this.tempPwrDataList.push(this.curr_temp_pwr);
+      this.totalPwrDataList.shift();
+      this.totalPwrDataList.push(this.curr_total_pwr);
+      this.updateTimeLines();
+      myChart1.update();
+      myChart2.update();
+      this.myChartData.update();
+    }, 2000);
 
   }
   
   public updateOptions() {
     this.myChartData.data.datasets[0].data = this.data;
     this.myChartData.update();
+  }
+
+  updateTimeLines() {
+    if(this.curr_time >= 58) {
+      this.curr_time = 0;
+    } else {
+      this.curr_time += 2;
+    }
+    this.timeLineList1.shift();
+    this.timeLineList1.push(this.curr_time);
+    this.timeLineList2.shift();
+    this.timeLineList2.push(this.curr_time);
+  }
+
+  chartNavigate() {
+    if(this.clicked || this.clicked1) {
+      this.router.navigate(["sensor-inputs"]);
+    }
   }
 }
