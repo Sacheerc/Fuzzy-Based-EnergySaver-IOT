403a502ed2a2dc4b22340068587493e77b9dff97
RavinduSachintha <ravindusachintha53@gmail.com>
2019-10-04 23:16:42 +0530

    fuzzy rules updated

--- RestApi/fuzzyApp/utils.py
+++ RestApi/fuzzyApp/utils.py
@@ -17,11 +17,17 @@ def fuzzy_val_generator(light_level_in,temp_level_in):
 
     # rules initialization
     rule1 = ctrl.Rule(light_level['poor'] & temp_level['poor'],(power_level_for_light['good'], power_level_for_temp['good']))
-    rule2 = ctrl.Rule(light_level['poor'] & temp_level['poor'],(power_level_for_light['good'], power_level_for_temp['good']))
-    rule3 = ctrl.Rule(light_level['poor'] & temp_level['poor'],(power_level_for_light['good'], power_level_for_temp['good']))
+    rule2 = ctrl.Rule(light_level['poor'] & temp_level['average'],(power_level_for_light['good'], power_level_for_temp['average']))
+    rule3 = ctrl.Rule(light_level['poor'] & temp_level['good'],(power_level_for_light['good'], power_level_for_temp['poor']))
+    rule4 = ctrl.Rule(light_level['average'] & temp_level['poor'],(power_level_for_light['average'], power_level_for_temp['good']))
+    rule5 = ctrl.Rule(light_level['average'] & temp_level['average'],(power_level_for_light['average'], power_level_for_temp['average']))
+    rule6 = ctrl.Rule(light_level['average'] & temp_level['good'],(power_level_for_light['average'], power_level_for_temp['poor']))
+    rule7 = ctrl.Rule(light_level['good'] & temp_level['poor'],(power_level_for_light['poor'], power_level_for_temp['good']))
+    rule8 = ctrl.Rule(light_level['good'] & temp_level['average'],(power_level_for_light['poor'], power_level_for_temp['average']))
+    rule9 = ctrl.Rule(light_level['good'] & temp_level['good'],(power_level_for_light['poor'], power_level_for_temp['poor']))
 
     # Control system and simulation
-    power_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
+    power_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9])
     power = ctrl.ControlSystemSimulation(power_ctrl)
 
     power.input['light_level'] = light_level_in
